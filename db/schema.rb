eac364ac256b91b3f64c9c38f649ef7a9aa52d0f
malikkcoleman <malikcoleman@live.ca>
2022-12-02 18:53:14 -0600

    Added activeadmin and categories/products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_02_212221) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_02_225401) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -37,4 +37,25 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_02_212221) do
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
   end
 
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.decimal "price"
+    t.text "description"
+    t.string "image"
+    t.integer "qty"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "category_id", null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
+  end
+
+  add_foreign_key "products", "categories"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,29 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+# AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+
+# require 'csv'
+# csv_text = File.read('/Users/colemancapsule/Documents/RRCWEBD3008/collectors_club_v3/db/csv/categories.csv')
+# puts csv_text
+# CSV.foreach('/Users/colemancapsule/Documents/RRCWEBD3008/collectors_club_v3/db/csv/categories.csv', headers: true) do |row|
+#   Category.create({
+#     name: row["name"],
+#     description: row["description"],
+#     image: row["image"]
+#   })
+# end
+
+require 'csv'
+csv_text = File.read('/Users/colemancapsule/Documents/RRCWEBD3008/collectors_club_v3/db/csv/products.csv')
+puts csv_text
+CSV.foreach('/Users/colemancapsule/Documents/RRCWEBD3008/collectors_club_v3/db/csv/products.csv', headers: true) do |row|
+  Product.create({
+    name: row["name"],
+    price: row["price"],
+    description: row["description"],
+    image: row["image"],
+    qty: row["qty"],
+    category_id: row["category_id"]
+  })
+end
