d53445c3d37e6e60abc4366bbfb654cff13fb124
malikkcoleman <malikcoleman@live.ca>
2022-12-12 14:27:05 -0600

    made the taxes dynamic to the province

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,10 +1,53 @@
 class OrdersController < ApplicationController
   def create
-    logger.debug("BUYING #{params[:name]} BUYING.")
+    tax = 1.0
+
+    if params[:province] == "AB"
+      tax = 1.05
+    end
+    if params[:province] == "BC"
+      tax = 1.12
+    end
+    if params[:province] == "MB"
+      tax = 1.12
+    end
+    if params[:province] == "NB"
+      tax = 1.15
+    end
+    if params[:province] == "NL"
+      tax = 1.15
+    end
+    if params[:province] == "NT"
+      tax = 1.05
+    end
+    if params[:province] == "NS"
+      tax = 1.15
+    end
+    if params[:province] == "NU"
+      tax = 1.05
+    end
+    if params[:province] == "ON"
+      tax = 1.13
+    end
+    if params[:province] == "PE"
+      tax = 1.15
+    end
+    if params[:province] == "QC"
+      tax = 1.14975
+    end
+    if params[:province] == "SK"
+      tax = 1.11
+    end
+    if params[:province] == "YT"
+      tax = 1.05
+    end
+
+    total = (params[:total].to_f * tax).round(2).to_s
+    logger.debug("BUYING #{params[:total].to_f * tax} BUYING.")
     @order = Order.new
     @order.name = params[:name]
     @order.province = params[:province]
-    @order.total = params[:total]
+    @order.total = total
     @order.save
     session[:shopping_cart].clear
 
