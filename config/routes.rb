cce642ce9ab05f9f14a092d89f0cd75121dfc6ed
malikkcoleman <malikcoleman@live.ca>
2022-12-12 21:11:29 -0600

    finishing up project

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get "orders/index"
   get "orders/show"
   devise_for :users
-  get "home/index"
+  get "/home", to: "home#index"
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
